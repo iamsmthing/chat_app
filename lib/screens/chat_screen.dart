55c819aed4e953ff9f3557720ddac06dfce21c4e
SHIVAM KUMAR RAI <90586668+iamsmthing@users.noreply.github.com>
2022-07-17 18:33:21 +0530

    Add files via upload

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,25 +13,76 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  List<Widget> screens = const [Chats(), Peoples(), Profile(), Setting()];
+  int _currentTabIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(items: const [
-        BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.chat_bubble), label: 'Chats'),
-        BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.person_2), label: 'Connections'),
-        BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.person), label: 'Profile'),
-        BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.settings), label: 'Settings')
-      ]),
-      tabBuilder: (BuildContext context, int index) {
-        return screens[index];
+    final _screens = <Widget>[
+      const Chats(),
+      const Peoples(),
+      const Profile(),
+      const Setting()
+    ];
+    //final tabPages = <Widget>[const Center(child: Icon(Icons.chat_bubble)),const Center(child: Icon(Icons.people)),const Center(child: Icon(Icons.person )),const Center(child: Icon(Icons.settings)) ];
+    final bottomNavBarItems = <BottomNavigationBarItem>[
+      const BottomNavigationBarItem(
+        icon: Icon(
+          CupertinoIcons.chat_bubble,
+          color: Colors.blue,
+        ),
+        label: 'Chats',
+      ),
+      const BottomNavigationBarItem(
+          icon: Icon(
+            CupertinoIcons.person_2,
+            color: Colors.blue,
+          ),
+          label: 'Connections'),
+      const BottomNavigationBarItem(
+          icon: Icon(
+            CupertinoIcons.person,
+            color: Colors.blue,
+          ),
+          label: 'Profile'),
+      const BottomNavigationBarItem(
+          icon: Icon(
+            CupertinoIcons.settings,
+            color: Colors.blue,
+          ),
+          label: 'Settings')
+    ];
+    assert(_screens.length == bottomNavBarItems.length);
+    final bottomNavBar = BottomNavigationBar(
+      fixedColor: Colors.black,
+      selectedLabelStyle: const TextStyle(color: Colors.black),
+      unselectedLabelStyle: const TextStyle(color: Colors.grey),
+      items: bottomNavBarItems,
+      currentIndex: _currentTabIndex,
+      type: BottomNavigationBarType.shifting,
+      onTap: (int index) {
+        setState(() {
+          _currentTabIndex = index;
+        });
       },
-    ));
+    );
+    return Scaffold(
+      // body: CupertinoTabScaffold(
+      //   tabBar: CupertinoTabBar(items: const [
+      // BottomNavigationBarItem(
+      //     icon: Icon(CupertinoIcons.chat_bubble), label: 'Chats'),
+      // BottomNavigationBarItem(
+      //     icon: Icon(CupertinoIcons.person_2), label: 'Connections'),
+      // BottomNavigationBarItem(
+      //     icon: Icon(CupertinoIcons.person), label: 'Profile'),
+      // BottomNavigationBarItem(
+      //     icon: Icon(CupertinoIcons.settings), label: 'Settings')
+      //   ]),
+      //   tabBuilder: (BuildContext context, int index) {
+      //     return screens[index];
+      //   },
+      // ),
+      body: _screens[_currentTabIndex],
+      bottomNavigationBar: bottomNavBar,
+    );
   }
 }
--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -9,68 +9,74 @@ class Chats extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('chats').snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return const Center(
-            child: Text('Error Occured'),
-          );
-        }
-        if (snapshot.hasData) {
-          return CustomScrollView(
-            slivers: [
-              const CupertinoSliverNavigationBar(
-                largeTitle: Text('Chats'),
-              ),
-              SliverList(
-                delegate: SliverChildListDelegate(
-                  snapshot.data!.docs.map((DocumentSnapshot document) {
-                    Map<String, dynamic>? data =
-                        document.data() as Map<String, dynamic>?;
-                    //return Text(data!['text']);
-                    return Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              CupertinoPageRoute(
-                                  builder: (context) => UserChatScreen()));
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.only(
-                              left: 5, right: 5, top: 15, bottom: 15),
-                          decoration: BoxDecoration(
-                              color: Colors.transparent,
-                              borderRadius: BorderRadius.circular(15.0)),
-                          child: Row(
-                            children: [
-                              CircleAvatar(child: Text(data!['name'][0])),
-                              const SizedBox(width: 7.0),
-                              Column(
-                                children: [
-                                  Text(
-                                    data['name'],
-                                    style: const TextStyle(fontSize: 22.0),
-                                  ),
-                                  Text(data['text'],
-                                      style: const TextStyle(fontSize: 14.0)),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
-                  }).toList(),
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 240, 243, 243),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('chats').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Error Occured'),
+            );
+          }
+          if (snapshot.hasData) {
+            return CustomScrollView(
+              slivers: [
+                const CupertinoSliverNavigationBar(
+                  largeTitle: Text('Chats'),
                 ),
-              )
-            ],
-          );
-        }
-        return Container();
-      },
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    snapshot.data!.docs.map(
+                      (DocumentSnapshot document) {
+                        Map<String, dynamic>? data =
+                            document.data() as Map<String, dynamic>?;
+                        //return Text(data!['text']);
+                        return Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  CupertinoPageRoute(
+                                      builder: (context) => UserChatScreen()));
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.only(
+                                  left: 5, right: 5, top: 15, bottom: 15),
+                              decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: BorderRadius.circular(15.0)),
+                              child: Row(
+                                children: [
+                                  CircleAvatar(child: Text(data!['name'][0])),
+                                  const SizedBox(width: 7.0),
+                                  Column(
+                                    children: [
+                                      Text(
+                                        data['name'],
+                                        style: const TextStyle(fontSize: 22.0),
+                                      ),
+                                      Text(data['text'],
+                                          style:
+                                              const TextStyle(fontSize: 14.0)),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ).toList(),
+                  ),
+                )
+              ],
+            );
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
